57b4f162a78dfd5e7ed76d7d29926c101fb2bdb3
Jorgearellano1 <129127690+Jorgearellano1@users.noreply.github.com>
2024-03-05 12:06:24 -0500

    Add files via upload

---
+++ asd/microservicio-autenticacion/src/test/java/com/example/microservicioautenticacion/MicroservicioAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicioautenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
